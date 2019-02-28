b7685d4827bb7567bb4c05ee52d2952d2490e3cb
Beaky <retomlinson14@gmail.com>
2019-02-28 11:52:00 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'aej1DxtITp7ez5ZN8yuLhJAasOoz5k5e';
+
+	var map = L.mapquest.map('map', {
+		center: [32.8794042,-117.2353702],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.8794042,-117.2353702]).addTo(map);
 }
